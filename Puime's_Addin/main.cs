6f5da97d953fa7106ea01e30e2f688da5696c841
esu01142 <sergio.puime@es.abb.com>
2023-06-30 14:24:16 +0200

    2023.06.30

--- Puime's_Addin/main.cs
+++ Puime's_Addin/main.cs
@@ -30,7 +30,7 @@ namespace Puime_s_Addin
 
         public static void AddinMain()
         {
-            Logger.AddMessage(new LogMessage("Puime's Addin Loaded ... 2023/06/21 - 9:39", "Puime's Add-in"));
+            Logger.AddMessage(new LogMessage("Puime's Addin Loaded ... 2023/06/27 - 12:31", "Puime's Add-in"));
 
             if (rgPA == null)
             {
