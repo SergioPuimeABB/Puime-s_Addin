25c415fd967a1206d359cab65121b745c8a10e46
Sergio Puime Gil - ABB - <53864256+SergioPuimeABB@users.noreply.github.com>
2024-08-20 14:08:48 +0200

    2024.08.20

--- Puime's_Addin/frmZoom.cs
+++ Puime's_Addin/frmZoom.cs
@@ -1,27 +1,13 @@
 ﻿using ABB.Robotics.RobotStudio.Stations.Forms;
 using ABB.Robotics.RobotStudio;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using ABB.Robotics.Math;
-using ABB.Robotics.RobotStudio.Environment;
-using ABB.Robotics.RobotStudio.Stations;
-using PuimesAddin.Properties;
-
 
 
 namespace PuimesAddin
 {
     public partial class frmZoom : Form
     {
-
         float zoomfactor = 10;
 
         public frmZoom()
@@ -29,15 +15,11 @@ namespace PuimesAddin
             InitializeComponent();
         }
 
-        
-
         private void btn_more_Click(object sender, EventArgs e)
         {
             Project.UndoContext.BeginUndoStep("Zoom");
             try
             {
-                //TODO:
-                //Igual hacer ventana con más y menos
                 GraphicControl.ActiveGraphicControl.Zoom(zoomfactor, 0.2F);
             }
             catch (Exception execption)
@@ -57,8 +39,6 @@ namespace PuimesAddin
             Project.UndoContext.BeginUndoStep("Zoom");
             try
             {
-                //TODO:
-                //Igual hacer ventana con más y menos
                 GraphicControl.ActiveGraphicControl.Zoom(-zoomfactor, 0.2F);
             }
             catch (Exception execption)
